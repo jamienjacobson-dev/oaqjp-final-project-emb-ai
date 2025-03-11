76f55318036ba6c11386019807ff4021bcaad99f
Jamie Jacobson <jamienjacobson@outlook.com>
2025-03-11 08:28:43 -0400

    Static Code analysis modifications

--- final_project/server.py
+++ final_project/server.py
@@ -1,3 +1,4 @@
+""" Python Flask AI Application: Emotion Detctor App"""
 from flask import Flask, render_template, request, make_response
 from EmotionDetection import emotion_detector
 
@@ -5,23 +6,25 @@ app = Flask("EmotionDetectionApp",template_folder='../templates',static_folder="
 
 @app.route("/emotionDetector")
 def get_emotions():
+    """Return emotional analysis for a given string"""
     text_to_analyze = request.args.get('textToAnalyze')
 
     if not text_to_analyze:
         return " Invalid text! Please try again!."
 
     emotions = emotion_detector(text_to_analyze)
-    outString = "For the given statement, the system response is "
-    outString += f"'anger': {emotions['anger']}, "
-    outString += f"'disgust': {emotions['disgust']}, "
-    outString += f"'fear': {emotions['fear']}, "
-    outString += f"'joy': {emotions['joy']} and "
-    outString += f"'sadness': {emotions['sadness']}. "
-    outString += f"The dominent emotion is <b>{emotions['dominant_emotion']}<b>"
-    return make_response(outString)
+    out_string = "For the given statement, the system response is "
+    out_string += f"'anger': {emotions['anger']}, "
+    out_string += f"'disgust': {emotions['disgust']}, "
+    out_string += f"'fear': {emotions['fear']}, "
+    out_string += f"'joy': {emotions['joy']} and "
+    out_string += f"'sadness': {emotions['sadness']}. "
+    out_string += f"The dominent emotion is <b>{emotions['dominant_emotion']}<b>"
+    return make_response(out_string)
 
 @app.route("/")
 def render_index_page():
+    """Display home page"""
     return render_template('index.html')
 
 if __name__ == "__main__":
